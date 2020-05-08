b5832038f894b1bf39b6e8ae6aa77320c7a70ee4
MsLulia <adriana.hernandez605@gmail.com>
2020-05-08 18:55:54 -0500

    primer commit

---
+++ keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'sql10.freemysqlhosting.net',
+        user: 'sql10338098',
+        password: 'PUmepMFd6u',
+        database: 'sql10338098'
+    }
+
+};
